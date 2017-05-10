3894bc9fd0cfc1ea2f2562c207e6ea68d3a01d08
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2017-05-10 17:39:12 +0200

    release: release version

---
+++ dist/index.js
@@ -0,0 +1,89 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Mickael Jeanroy
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+'use strict';
+
+var _ = require('lodash');
+var MagicString = require('magic-string');
+var diff = require('diff');
+var esformatter = require('esformatter');
+
+module.exports = function (options) {
+  var sourceMap = void 0;
+
+  return {
+    /**
+     * Function called by `rollup` that is used to read the `sourceMap` setting.
+     *
+     * @param {Object} opts Rollup options.
+     * @return {void}
+     */
+    options: function options(opts) {
+      sourceMap = !!opts && !!opts.sourceMap;
+    },
+
+
+    /**
+     * Function called by `rollup` before generating final bundle.
+     *
+     * @param {string} source Souce code of the final bundle.
+     * @return {Object} The result containing a `code` property and, if source map is enabled,
+     *                  a `map` property.
+     */
+    transformBundle: function transformBundle(source) {
+      var output = esformatter.format(source, options);
+
+      // No need to do more.
+      if (!sourceMap) {
+        return { code: output };
+      }
+
+      console.log('[rollup-plugin-esformatter] Source-map is enabled, computing diff is required');
+      console.log('[rollup-plugin-esformatter] This may take a moment (depends on the size of your bundle)');
+
+      var magicString = new MagicString(source);
+      var changes = diff.diffChars(source, output);
+
+      var idx = 0;
+
+      _.forEach(changes, function (part) {
+        if (part.added) {
+          magicString.prependLeft(idx, part.value);
+          idx -= part.count;
+        } else if (part.removed) {
+          magicString.remove(idx, idx + part.count);
+        }
+
+        idx += part.count;
+      });
+
+      return {
+        code: magicString.toString(),
+        map: magicString.generateMap({
+          hires: true
+        })
+      };
+    }
+  };
+};
