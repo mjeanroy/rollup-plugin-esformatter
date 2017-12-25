ff7a81b3b237517d83954a6b0974522ec7b75635
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2017-12-25 11:05:40 +0100

    core: fix release task

--- gulpfile.js
+++ gulpfile.js
@@ -96,8 +96,8 @@ gulp.task('tag', (done) => {
 ['major', 'minor', 'patch'].forEach((level) => {
   gulp.task(`bump:${level}`, () => {
     return gulp.src(path.join(__dirname, 'package.json'))
-      .pipe(bump({type: level})
-      .on('error', (e) => log.error(e)))
+      .pipe(bump({type: level}))
+      .on('error', (e) => log.error(e))
       .pipe(gulp.dest(__dirname));
   });
 
